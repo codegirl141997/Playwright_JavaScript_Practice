1f8a99ba944e272b3b68d6937bfd82c6a7b4ed29
codegirl1411997 <dhanashreee14@gmail.com>
2026-07-02 10:53:09 +0530

    updated

--- tests/UI_Basic.spec.js
+++ tests/UI_Basic.spec.js
@@ -1,11 +1,28 @@
-const {test}= require('@playwright/test');
+const {test, expect}= require('@playwright/test');
 
 // test('first playwright test', async function()
-
-test('first playwright test', async ()=>
+// 1st way we can write this 
+test('first playwright test', async ({browser, page})=>
 // in Javascript not execute sequnce 
 
 {
+ 
+    // chrome - Playwright/ cookies 
 
-
+    //const context = browser.newContext(); // fresh browser no cookies instance
+    //const page = await context.newPage(); // New page will open
+    await page.goto("https://www.google.com");
+          console.log(await page.title)
+     await expect(page).toHaveTitle("Google");
 });
+
+ 
+//2nd way we can write this 
+test.only('Rahul_Shetty', async({browser,page})=>
+
+    {
+      await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+      await page.locator('#username').type("rahulshettyacademy ");
+      await page.locator("#password").type("Learning@830$3mK2)");
+      await page.locator("#signInBtn").click();
+    });
