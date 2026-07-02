4b3bb2c299e0ab04485588de6288b3d5ccab180a
codegirl1411997 <dhanashreee14@gmail.com>
2026-07-02 14:36:01 +0530

    Dropdown_Practice

--- tests/SauceDemo.spec.js
+++ tests/SauceDemo.spec.js
@@ -7,4 +7,5 @@ test('SauceDemo', async ({ browser, page }) => {
 await page.locator('#user-name').fill("standard_user");
 await page.locator('#password').fill("secret_sauce");
 await page.locator('#login-button').click();
+await page.locator('add-to-cart-sauce-labs-backpack').textContent();
 });
--- tests/UI_Basic.spec.js
+++ tests/UI_Basic.spec.js
@@ -20,7 +20,25 @@
  
 //2nd way we can write this 
 //const { test, expect } = require('@playwright/test');
+/*
+const {test, expect}= require('@playwright/test');
+test('Rahul_Shetty', async ({browser, page }) => {
 
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    await page.locator("#username").fill("scsbcskcls");
+    await page.locator("#password").fill("Learning");
+
+    await page.locator("#signInBtn").click();
+
+    console.log(await page.locator("[style*='block']").textContent());
+
+    await expect(page.locator("[style*='block']")).toContainText("Incofrrect");
+});
+*/
+
+/** 
+const {test, expect}= require('@playwright/test');
 test('Rahul_Shetty', async ({browser, page }) => {
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
@@ -34,3 +52,22 @@ test('Rahul_Shetty', async ({browser, page }) => {
 
     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 });
+*/
+
+//type & Fil methods which is used to fill the info the text box
+// decleare locators
+const{test, expect} = require('@playwright/test');
+test ('Rahul_Shetty' ,async ({browser,page})=>
+{
+  const username = page.locator("#username");
+  const password = page.locator("#password");
+  const Submit_button = page.locator("#signInBtn");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  
+  await username.type("rahulshettyacademy ");
+  await password.type("Learning@830$3mK2");
+  await Submit_button.click();
+});
+
